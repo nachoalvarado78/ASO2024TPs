c0c3aff9016d9b13b9b3caace74276e933c1d6f0
nachoalvarado78 <ignacioalvarado230804@gmail.com>
2024-05-16 18:21:18 -0300

    listo

--- TP3/punto2/con_race_condition.c
+++ TP3/punto2/con_race_condition.c
@@ -10,22 +10,21 @@ void *comer_hamburguesa(void *tid)
 {
 	while (1 == 1)
 	{ 
-	
+      while(turno!=(int)tid);
     // INICIO DE LA ZONA CRÍTICA
-     while(turno!=(int)tid); 
 		if (cantidad_restante_hamburguesas > 0)
 		{
 			printf("Hola! soy el hilo(comensal) %d , me voy a comer una hamburguesa ! ya que todavia queda/n %d \n", (int) tid, cantidad_restante_hamburguesas);
 			cantidad_restante_hamburguesas--; // me como una hamburguesa
-		}
+		} // LA REGION CRITICA SE REFIERE A EL MOMENTO DONDE SE COME LA HAMBURGUESA
 		else
 		{
 			printf("SE TERMINARON LAS HAMBURGUESAS :( \n");
-
+            turno = (turno + 1)% NUMBER_OF_THREADS;
 			pthread_exit(NULL); // forzar terminacion del hilo
 		}
-    // SALIDA DE LA ZONA CRÍTICA   
-	turno = (turno + 1)% NUMBER_OF_THREADS;
+       // SALIDA DE LA ZONA CRÍTICA 
+	  turno = (turno + 1)% NUMBER_OF_THREADS;
 	}
 }
 
@@ -33,7 +32,6 @@ int main(int argc, char *argv[])
 {
 	pthread_t threads[NUMBER_OF_THREADS];
 	int status, i, ret;
-    
 	for (int i = 0; i < NUMBER_OF_THREADS; i++)
 	{
 		printf("Hola!, soy el hilo principal. Estoy creando el hilo %d \n", i);
